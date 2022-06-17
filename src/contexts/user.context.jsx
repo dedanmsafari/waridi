1bca8d91ad4da000bc446c1360596a6c239e2691
dedanmsafari <dedan.msafari@gmail.com>
2022-06-17 09:58:52 +0300

    modified authentication and created user context

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState, useEffect } from "react";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentfromAuth,
+} from "../utils/firebase/firebase.utils";
+
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+
+const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentfromAuth(user);
+      }
+      setCurrentUser(user);
+      console.log(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+export default UserProvider;
