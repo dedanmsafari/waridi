59758c2bb7727bce8947a96b23094eda48a717b0
dedanmsafari <dedan.msafari@gmail.com>
2022-06-17 09:57:52 +0300

    added user provider context

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 import App from "./App";
+import UserProvider from "../src/contexts/user.context";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
