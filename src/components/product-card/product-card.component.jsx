1cd79ed35e556692dba3cdd93e821742477b5b24
dedanmsafari <dedan.msafari@gmail.com>
2022-06-21 09:19:12 +0300

    product card component

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { CartDropdownContext } from "../../contexts/cartDropdown.context";
+import Button from "../button/button.component";
+
+import "./product-card.styles.scss";
+
+const ProductCard = ({ product }) => {
+  const { name, imageUrl, price } = product;
+  const { addItemToCart } = useContext(CartDropdownContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        ADD TO CART
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCard;
