4be93b7e9561beca1b4c3cd9758299097053cb60
dedanmsafari <dedan.msafari@gmail.com>
2022-06-22 09:06:48 +0300

    cart item component

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CartItem = ({ product }) => {
+  const { name, quantity } = product;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <span>{quantity}</span>
+    </div>
+  );
+};
+
+export default CartItem;
