8eb359c4f6b52bd61c2946214c17b2194f5c7693
dedanmsafari <dedan.msafari@gmail.com>
2022-07-19 18:39:09 +0300

    created our user reducer

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,22 @@
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const UserActionType = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case UserActionType.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      return state;
+  }
+};
