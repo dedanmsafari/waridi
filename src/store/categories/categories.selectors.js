f98d938713dac47292e0f4d73cad89ae045332e0
dedanmsafari <dedan.msafari@gmail.com>
2022-07-25 21:31:39 +0300

    created categories selector

---
+++ src/store/categories/categories.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from "reselect";
+
+const categoriesSelector = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [categoriesSelector],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+const toObject = (acc, categoryItem) => {
+  const { title, items } = categoryItem;
+  acc[title.toLowerCase()] = items;
+  return acc;
+};
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+
+  (categories) => categories.reduce(toObject, {})
+);
