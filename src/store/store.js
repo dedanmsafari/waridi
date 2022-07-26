2fb17f499ec866c22f92ee1ce77ae046a7072cf1
dedanmsafari <dedan.msafari@gmail.com>
2022-07-26 04:41:34 +0300

    created the store

--- src/store/store.js
+++ src/store/store.js
@@ -5,9 +5,23 @@ import {
 } from "redux";
 import logger from "redux-logger";
 
-import { rootReducer } from "../store/rootReducer";
+import { rootReducer } from "./rootReducer";
 
-const middleWares = [logger];
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type:", action.type);
+  console.log("payload:", action.payload);
+  console.log("currentState:", store.getState());
+
+  next(action);
+
+  console.log("nextState:", store.getState());
+};
+
+const middleWares = [loggerMiddleWare];
+
 const composedEnhansers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhansers);
