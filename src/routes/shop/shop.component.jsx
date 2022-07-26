73eff6a129e666b08ecf200bf6e4c348031d31f0
dedanmsafari <dedan.msafari@gmail.com>
2022-07-26 04:42:49 +0300

    modified shop to use redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { useDispatch } from "react-redux";
+
+import { setCategories } from "../../store/categories/categories.actions";
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoriesArray = await getCategoriesAndDocuments();
+
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategories();
+  }, [dispatch]);
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
