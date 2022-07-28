3b70448ce0bd523051957ac2f0fd69fda72511a6
dedanmsafari <dedan.msafari@gmail.com>
2022-07-28 09:55:57 +0300

    modified the category component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,15 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { useSelector } from "react-redux";
 
 import "./category.styles.scss";
+import { selectCategoriesMap } from "../../store/categories/categories.selectors";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
