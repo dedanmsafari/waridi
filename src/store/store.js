22f9df01325fd7b95d9e3a71174be3206c1754ff
dedanmsafari <dedan.msafari@gmail.com>
2022-07-29 18:19:53 +0300

    modified our store to redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -7,33 +7,30 @@ import {
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./rootReducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
-const loggerMiddleWare = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type:", action.type);
-  console.log("payload:", action.payload);
-  console.log("currentState:", store.getState());
-
-  next(action);
-
-  console.log("nextState:", store.getState());
-};
-
-const middleWares = [loggerMiddleWare];
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhansers = compose(applyMiddleware(...middleWares));
+const composedEnhanser =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhansers = composedEnhanser(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
