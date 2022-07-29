5eb1fa5ec3b65da8273cd29ab9eef1584e21fa00
dedanmsafari <dedan.msafari@gmail.com>
2022-07-29 18:22:22 +0300

    added spinner component

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+export const Spinner = () => {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  );
+};
