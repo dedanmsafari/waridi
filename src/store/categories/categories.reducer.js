72fdcb4e5d23676f695c956490dbd8b1ab605bcc
dedanmsafari <dedan.msafari@gmail.com>
2022-07-29 18:18:28 +0300

    modified our reducer to thunks

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -2,16 +2,31 @@ import { CategoryTypes } from "./categories.actionTypes";
 
 const INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReducer = (state = INITIAL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CategoryTypes.SET_CATEGORIES:
+    case CategoryTypes.FETCH_CATEGORIES_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case CategoryTypes.FETCH_CATEGORIES_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload,
+      };
+
+    case CategoryTypes.FETCH_CATEGORIES_SUCCESS:
       return {
         ...state,
         categories: payload,
+        isLoading: false,
       };
 
     default:
