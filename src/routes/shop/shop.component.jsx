bd449f3692f337f16daf4ac113b3221374a671d8
dedanmsafari <dedan.msafari@gmail.com>
2022-07-30 18:29:42 +0300

    plain action dispatch to call saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesStart } from "../../store/categories/categories.actions";
 import { useDispatch } from "react-redux";
 
-import { fetchCategoriesAsync } from "../../store/categories/categories.actions";
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
@@ -11,7 +10,7 @@ import "./shop.styles.scss";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, [dispatch]);
 
   return (
