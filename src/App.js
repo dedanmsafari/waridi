d3cbe84b5694519b6b667b353583bdd8376a3217
dedanmsafari <dedan.msafari@gmail.com>
2022-07-30 21:03:55 +0300

    abstracted listener to app component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import {
   onAuthStateChangedListener,
   createUserDocumentfromAuth,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
 
 import { setCurrentUser } from "./store/user/user.actions";
@@ -19,14 +20,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentfromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, [dispatch]);
 
   return (
