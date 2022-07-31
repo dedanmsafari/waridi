5413b280576507040566acfcdd4822e51e28cc99
dedanmsafari <dedan.msafari@gmail.com>
2022-07-31 22:50:52 +0300

    corrections to t he user saga

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,16 +1,18 @@
-import { takeLatest, call, all, put } from "redux-saga";
+import { takeLatest, call, all, put } from "redux-saga/effects";
 import {
   createUserDocumentfromAuth,
   getCurrentUser,
 } from "../../utils/firebase/firebase.utils";
-import { signInFailed } from "./user.actions";
+import { signInFailed, signInSuccess } from "./user.actions";
 import { UserActionType } from "./user.actionTypes";
 
 export function* isUserAuthenticated() {
   try {
-    const userAuth = yield getCurrentUser;
+    const userAuth = yield call(getCurrentUser);
     if (!userAuth) return;
-    createUserDocumentfromAuth(userAuth);
+    //attach nexty line to a saga
+    const auth = yield call(createUserDocumentfromAuth(userAuth));
+    yield all([call(signInSuccess(auth))]);
   } catch (error) {
     yield put(signInFailed(error));
   }
