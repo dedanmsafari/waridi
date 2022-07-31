26828a200008e40088056ea91f54cbbf4226cb65
dedanmsafari <dedan.msafari@gmail.com>
2022-07-31 21:09:55 +0300

    modified the root saga

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,5 +1,6 @@
 import { call, all } from "redux-saga/effects";
 import { categoriesSaga } from "./categories/categories.saga";
+import { userSaga } from "./user/user.saga";
 
 export function* rootSaga() {
   yield all([call(categoriesSaga)]);
