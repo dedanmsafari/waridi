3f04fca31a99508227c7f0e17f8f1e6bc1e1d5bd
dedanmsafari <dedan.msafari@gmail.com>
2022-07-31 21:08:18 +0300

    modified the user reducer

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -2,16 +2,23 @@ import { UserActionType } from "./user.actionTypes";
 
 const INITIAL_STATE = {
   currentUser: null,
+  isLoading: false,
+  error: null,
 };
 
 export const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case UserActionType.SET_CURRENT_USER:
+    case UserActionType.SIGN_IN_SUCCESS:
       return {
         ...state,
         currentUser: payload,
+      };
+    case UserActionType.SIGN_IN_FAILED:
+      return {
+        ...state,
+        error: payload,
       };
 
     default:
