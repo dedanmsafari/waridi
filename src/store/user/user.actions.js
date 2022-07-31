89db179b09a3144418d5dba3459eb7f9505e40dd
dedanmsafari <dedan.msafari@gmail.com>
2022-07-31 21:09:14 +0300

    modified user actions to sagas

--- src/store/user/user.actions.js
+++ src/store/user/user.actions.js
@@ -3,3 +3,25 @@ import { createAction } from "../../utils/reducer/reducer.utils";
 
 export const setCurrentUser = (user) =>
   createAction(UserActionType.SET_CURRENT_USER, user);
+
+/*
+    SET_CURRENT_USER: "user/SET_CURRENT_USER",
+  GET_CURRENT_SESSION: "user/GET_CURRENT_SESSION",
+  SIGN_IN_GOOGLE: "user/SIGN_IN_GOOGLE",
+  SIGN_IN_EMAIL_PASSWORD: "user/SIGN_IN_EMAIL_PASSWORD",
+  SIGN_IN_SUCCESS: "user/SIGN_IN_SUCCESS",
+  SIGN_IN_FAILED: "USER/SIGN_IN_FAILED",
+  */
+export const signInGoogle = () => createAction(UserActionType.SIGN_IN_GOOGLE);
+
+export const signInWithEmailAndPassword = (email, password) =>
+  createAction(UserActionType.SIGN_IN_EMAIL_PASSWORD, { email, password });
+
+export const signInSuccess = (user) =>
+  createAction(UserActionType.SIGN_IN_SUCCESS, user);
+
+export const signInFailed = (error) =>
+  createAction(UserActionType.SIGN_IN_FAILED, error);
+
+export const getCurrentSession = () =>
+  createAction(UserActionType.GET_CURRENT_SESSION);
