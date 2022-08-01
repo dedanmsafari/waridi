8b91edd701a44654a840e2b642d5e0da62ab6bd7
dedanmsafari <dedan.msafari@gmail.com>
2022-08-01 14:36:55 +0300

    modified the root saga

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -3,5 +3,5 @@ import { categoriesSaga } from "./categories/categories.saga";
 import { userSaga } from "./user/user.saga";
 
 export function* rootSaga() {
-  yield all([call(categoriesSaga)]);
+  yield all([call(categoriesSaga), call(userSaga)]);
 }
