7fe92fc0591aaee32069c28939df65352ec1ff92
dedanmsafari <dedan.msafari@gmail.com>
2022-08-06 21:23:00 +0300

    payment form component change

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,25 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 export const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventdefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+  };
   return (
-    <>
-      <CardElement />
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
-    </>
+    <PaymentFormContainer>
+      <FormContainer>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
