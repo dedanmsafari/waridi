dff7699461e00bfebd079a5c814892e96cb849f7
dedanmsafari <dedan.msafari@gmail.com>
2022-08-07 18:47:56 +0300

    shopchange

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+
 import "./shop.styles.scss";
 
 const Shop = () => {
