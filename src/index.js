e7138cca8cccda93ae995ca4dc6e35d7d2a90721
dedanmsafari <dedan.msafari@gmail.com>
2022-08-07 18:45:54 +0300

    random index change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./utils/stripe/stripe";
 import "./index.css";
