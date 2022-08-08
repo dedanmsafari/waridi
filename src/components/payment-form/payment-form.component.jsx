f06278f28a061d60e81941b6bee4e2e0e933d0ff
dedanmsafari <dedan.msafari@gmail.com>
2022-08-08 20:09:55 +0300

    added space to payment form

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,6 +1,7 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
 import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 export const PaymentForm = () => {
