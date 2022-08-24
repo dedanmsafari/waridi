7b0d39dc3cd73d847dd817ffeab51aad71c0c1be
dedanmsafari <dedan.msafari@gmail.com>
2022-08-24 08:51:18 +0300

    cart reducer file

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -3,69 +3,16 @@ import { CartTypes } from "./cart.actionTypes";
 const INITIAL_STATE = {
   openDropdown: false,
   cartItems: [],
-  cartTotal: 0,
-  totalCost: 0,
 };
 
 export const cartReducer = (state = INITIAL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CartTypes.ADD_ITEM_TO_CART:
-      const existingItem = state.cartItems.find(
-        (item) => item.id === payload.id
-      );
-
-      if (existingItem) {
-        const updatedCart = state.cartItems.map((item) => {
-          if (item.id === payload.id) {
-            return { ...item, quantity: item.quantity + 1 };
-          } else {
-            return item;
-          }
-        });
-        return { ...state, cartItems: updatedCart };
-      } else {
-        const updatedCart = [...state.cartItems, { ...payload, quantity: 1 }];
-
-        return { ...state, cartItems: updatedCart };
-      }
-
-    case CartTypes.REMOVE_FROM_CART:
-      const remainingProducts = state.cartItems.filter(
-        (cartItem) => cartItem !== payload
-      );
+    case CartTypes.SET_CART_ITEMS:
       return {
         ...state,
-        cartItems: remainingProducts,
-      };
-
-    case CartTypes.DECREASE_FROM_CART:
-      const includedItem = state.cartItems.find(
-        (item) => item.id === payload.id
-      );
-
-      if (includedItem && includedItem.quantity === 1) {
-        const savedProducts = state.cartItems.filter(
-          (cartItem) => cartItem !== includedItem
-        );
-
-        return {
-          ...state,
-          cartItems: savedProducts,
-        };
-      }
-
-      const decreasedCart = state.cartItems.map((item) => {
-        if (item.id === payload.id) {
-          return { ...item, quantity: item.quantity - 1 };
-        } else {
-          return item;
-        }
-      });
-      return {
-        ...state,
-        cartItems: decreasedCart,
+        cartItems: payload,
       };
 
     case CartTypes.OPEN_CART_DROPDOWN:
