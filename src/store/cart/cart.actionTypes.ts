c325b6f158846aaaf77b408eb55f1c9020221469
dedanmsafari <dedan.msafari@gmail.com>
2022-08-24 08:51:04 +0300

    cart actions type

--- src/store/cart/cart.actionTypes.ts
+++ src/store/cart/cart.actionTypes.ts
@@ -1,9 +1,7 @@
 import { CategoryItem } from "../categories/categories.actionTypes";
 
 export enum CartTypes {
-  ADD_ITEM_TO_CART = "cart/ADD_ITEM_TO_CART",
-  REMOVE_FROM_CART = "cart/REMOVE_FROM_CART",
-  DECREASE_FROM_CART = " cart/DECREASE_FROM_CART",
+  SET_CART_ITEMS = "cart/SET_CART_ITEMS",
   OPEN_CART_DROPDOWN = "cart/OPEN_CART_DROPDOWN",
 }
 
