19a48ae4ef12b0441355191ebc47a5268430683e
dedanmsafari <dedan.msafari@gmail.com>
2022-09-16 10:00:26 +0300

    modified the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,10 @@ import {
   compose,
   applyMiddleware,
   legacy_createStore as createStore,
+  Middleware,
 } from "redux";
 
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 //import thunk from "redux-thunk";
@@ -12,7 +13,18 @@ import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./rootReducer";
 import { rootSaga } from "./rootSaga";
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>;
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
@@ -23,7 +35,7 @@ const sagaMiddleWare = createSagaMiddleware();
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
   sagaMiddleWare,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
