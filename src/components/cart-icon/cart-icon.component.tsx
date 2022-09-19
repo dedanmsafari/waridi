9e42c9718373ab00b91778a729deee97a70793ed
dedanmsafari <dedan.msafari@gmail.com>
2022-09-19 10:59:07 +0300

    cart icon to typescript

---
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { CartContainer, ItemCount, ShoppingIcon } from "./cart-icon.styles.js";
+
+export type CartIconProps = {
+  cartTotal: number;
+};
+
+const CartIcon = ({ cartTotal }: CartIconProps) => {
+  return (
+    <CartContainer>
+      <ShoppingIcon className="shopping-icon" />
+      <ItemCount className="item-count">{cartTotal}</ItemCount>
+    </CartContainer>
+  );
+};
+
+export default CartIcon;
